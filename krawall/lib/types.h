2cbbe33ce4aed4cd62bff00b0eee7ced929e5ab9
Laurent Lardinois <19912792+type-one@users.noreply.github.com>
2022-04-16 00:26:13 +0200

    krawall with specific compilation options

---
+++ krawall/lib/types.h
@@ -0,0 +1,44 @@
+/*
+ * Krawall, XM/S3M Modplayer Library
+ * Copyright (C) 2001-2005, 2013 Sebastian Kienzl
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License in COPYING for more details.
+ */
+
+#ifndef __TYPES_H__
+#define __TYPES_H__
+
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned long u32;
+
+typedef signed char s8;
+typedef signed short s16;
+typedef signed long s32;
+
+#ifndef _SYS_TYPES_H
+typedef unsigned int uint;
+#endif
+
+#define EWRAM __attribute__((section(".ewram")))
+/*
+NOTE:
+If we'd force the section to .iwram here, we would get "section conflict" errors
+from GCC in some files as it doesn't seem to like code and data in the same section (any more?).
+But the default linker-script from devkitPro puts .data and .bss to iwram anyway, so this
+is not needed.
+*/
+#define IWRAM /*__attribute__((section(".iwram")))*/
+
+#define IWRAM_CODE __attribute__((section (".iwram"),long_call))
+#define LONG_CALL __attribute__((long_call))
+
+#endif
